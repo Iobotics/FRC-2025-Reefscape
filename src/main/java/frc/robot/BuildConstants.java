66ec0ee6d2582091084b776ac23e026dead61511
leungbrayden <leungbrayden@gmail.com>
2025-01-17 16:35:50 -1000

    switched to 2 motors instead

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "08f6eb0bfe9a6cf318e0070ee80ff8871329556d";
-  public static final String GIT_DATE = "2025-01-10 22:24:32 EST";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "f4b2b03e853577d346a427acbc2edf372072f9ed";
+  public static final String GIT_DATE = "2025-01-11 22:45:42 EST";
   public static final String GIT_BRANCH = "coral_manipulator";
-  public static final String BUILD_DATE = "2025-01-11 22:19:18 EST";
-  public static final long BUILD_UNIX_TIME = 1736651958124L;
+  public static final String BUILD_DATE = "2025-01-17 21:34:34 EST";
+  public static final long BUILD_UNIX_TIME = 1737167674443L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/subsystems/CoralManipulator/CoralManipulator.java
+++ src/main/java/frc/robot/subsystems/CoralManipulator/CoralManipulator.java
@@ -17,60 +17,37 @@ public class CoralManipulator extends SubsystemBase {
   /** Creates a new ExampleSubsystem. */
   private SparkMax TopleftOutake;
 
-  private SparkMax ToprightOutake;
   private static SparkClosedLoopController TopleftClosedLoopController;
-  private SparkClosedLoopController ToprightClosedLoopController;
   private RelativeEncoder TopleftOutakeEncoder;
-  private RelativeEncoder ToprightOutakeEncoder;
 
   private SparkMax BottomleftOutake;
-  private SparkMax BottomrightOutake;
   private static SparkClosedLoopController BottomleftClosedLoopController;
-  private SparkClosedLoopController BottomrightClosedLoopController;
   private RelativeEncoder BottomleftOutakeEncoder;
-  private RelativeEncoder BottomrightOutakeEncoder;
 
   private static final double kP = 0.0;
   private static final double kI = 0.0;
   private static final double kD = 0.0;
   private static final double kMaxOutput = 1.0;
   private static final double kMinOutput = -1.0;
-  /*
-    public final CoralManipulatorIO io;
-    public final
 
-  */
   public CoralManipulator(CoralManipulatorIO coralManipulatorIOSpark) {
     TopleftOutake = new SparkMax(1, MotorType.kBrushless);
-    ToprightOutake = new SparkMax(2, MotorType.kBrushless);
     TopleftClosedLoopController = TopleftOutake.getClosedLoopController();
-    ToprightClosedLoopController = ToprightOutake.getClosedLoopController();
     TopleftOutakeEncoder = TopleftOutake.getEncoder();
-    ToprightOutakeEncoder = ToprightOutake.getEncoder();
 
     BottomleftOutake = new SparkMax(1, MotorType.kBrushless);
-    BottomrightOutake = new SparkMax(2, MotorType.kBrushless);
     BottomleftClosedLoopController = BottomleftOutake.getClosedLoopController();
-    BottomrightClosedLoopController = BottomrightOutake.getClosedLoopController();
     BottomleftOutakeEncoder = BottomleftOutake.getEncoder();
-    BottomrightOutakeEncoder = BottomrightOutake.getEncoder();
 
     // top left config
     SparkMaxConfig TopleftConfig = new SparkMaxConfig();
 
-    TopleftConfig.inverted(true).idleMode(IdleMode.kCoast);
+    TopleftConfig.inverted(false).idleMode(IdleMode.kCoast);
     TopleftConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(kP, kI, kD);
 
     TopleftOutake.configure(
         TopleftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    // top right config
-    SparkMaxConfig ToprightConfig = new SparkMaxConfig();
 
-    ToprightConfig.inverted(false).idleMode(IdleMode.kCoast);
-    ToprightConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(kP, kI, kD);
-
-    ToprightOutake.configure(
-        ToprightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
     // bottom left config
     SparkMaxConfig BottomleftConfig = new SparkMaxConfig();
 
@@ -79,14 +56,6 @@ public class CoralManipulator extends SubsystemBase {
 
     BottomleftOutake.configure(
         BottomleftConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-    // bottom right config
-    SparkMaxConfig BottomrightConfig = new SparkMaxConfig();
-
-    BottomrightConfig.inverted(true).idleMode(IdleMode.kCoast);
-    BottomrightConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).pid(kP, kI, kD);
-
-    BottomrightOutake.configure(
-        BottomrightConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   public CoralManipulator(CoralManipulatorIOSim coralManipulatorIOSpark) {
@@ -94,22 +63,18 @@ public class CoralManipulator extends SubsystemBase {
   }
 
   public void setTopOutakeSpeed(double targetRPM) {
-    ToprightClosedLoopController.setReference(targetRPM, ControlType.kVelocity);
     TopleftClosedLoopController.setReference(targetRPM, ControlType.kVelocity);
     SmartDashboard.putNumber("Manipulator Target RPM", targetRPM);
   }
 
   public void setBottomOutakeSpeed(double targetRPM) {
-    BottomrightClosedLoopController.setReference(targetRPM, ControlType.kVelocity);
     BottomleftClosedLoopController.setReference(targetRPM, ControlType.kVelocity);
     SmartDashboard.putNumber("Manipulator Target RPM", targetRPM);
   }
 
   public void stopOutake() {
     TopleftOutake.set(0);
-    ToprightOutake.set(0);
     BottomleftOutake.set(0);
-    BottomrightOutake.set(0);
     SmartDashboard.putNumber("Manipulator Target RPM", 0);
   }
 
@@ -120,15 +85,7 @@ public class CoralManipulator extends SubsystemBase {
     double TopleftRPM = TopleftOutakeEncoder.getVelocity();
     SmartDashboard.putNumber("top left Manipulator Current RPM", TopleftRPM);
 
-    // Display secondary shooter RPM
-    double ToprightRPM = ToprightOutakeEncoder.getVelocity();
-    SmartDashboard.putNumber("top right Manipulator RPM", ToprightRPM);
-
     double BottomleftRPM = BottomleftOutakeEncoder.getVelocity();
     SmartDashboard.putNumber("bottom left Manipulator Current RPM", BottomleftRPM);
-
-    // Display secondary shooter RPM
-    double BottomrightRPM = BottomrightOutakeEncoder.getVelocity();
-    SmartDashboard.putNumber("bottom right Manipulator RPM", BottomrightRPM);
   }
 }
