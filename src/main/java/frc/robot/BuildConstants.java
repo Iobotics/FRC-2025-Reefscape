ed9aba919336b343d6316387b095f0cd73f6cc0c
leungbrayden <leungbrayden@gmail.com>
2025-01-17 16:55:38 -1000

    motor change; change from 4 to 2

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 12;
-  public static final String GIT_SHA = "f4b2b03e853577d346a427acbc2edf372072f9ed";
-  public static final String GIT_DATE = "2025-01-11 22:45:42 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "66ec0ee6d2582091084b776ac23e026dead61511";
+  public static final String GIT_DATE = "2025-01-17 21:35:50 EST";
   public static final String GIT_BRANCH = "coral_manipulator";
-  public static final String BUILD_DATE = "2025-01-17 21:34:34 EST";
-  public static final long BUILD_UNIX_TIME = 1737167674443L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-17 21:39:52 EST";
+  public static final long BUILD_UNIX_TIME = 1737167992459L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
