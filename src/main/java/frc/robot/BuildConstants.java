b8af165741aed902ec0cd8018bdfe8bc568d8061
Shane Matsushima <shanematsushima@gmail.com>
2025-01-18 14:33:52 -1000

    updated

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "08f6eb0bfe9a6cf318e0070ee80ff8871329556d";
-  public static final String GIT_DATE = "2025-01-10 22:24:32 EST";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "f67b00be3a22c6b173a4312260ea27fbee9c2a23";
+  public static final String GIT_DATE = "2025-01-14 13:59:03 EST";
   public static final String GIT_BRANCH = "coral_manipulator";
-  public static final String BUILD_DATE = "2025-01-11 22:19:18 EST";
-  public static final long BUILD_UNIX_TIME = 1736651958124L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2025-01-17 21:53:44 EST";
+  public static final long BUILD_UNIX_TIME = 1737168824447L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
