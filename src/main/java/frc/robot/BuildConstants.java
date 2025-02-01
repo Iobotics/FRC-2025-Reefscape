4ba7b2b5f8373f35e7a140f864517f7da485cccd
leungbrayden <leungbrayden@gmail.com>
2025-02-01 13:42:52 -1000

    swerve retune

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 51;
-  public static final String GIT_SHA = "1a89a63e59eff22fccd90b9035babae62380dc9a";
-  public static final String GIT_DATE = "2025-01-30 19:11:30 EST";
+  public static final int GIT_REVISION = 52;
+  public static final String GIT_SHA = "7a4b953d9510823e01d1571bea7e66724b1c8cbc";
+  public static final String GIT_DATE = "2025-01-30 22:23:46 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-30 22:18:57 EST";
-  public static final long BUILD_UNIX_TIME = 1738293537562L;
+  public static final String BUILD_DATE = "2025-02-01 18:36:50 EST";
+  public static final long BUILD_UNIX_TIME = 1738453010949L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
