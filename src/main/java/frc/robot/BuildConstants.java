92c7e98eb6c30711437e8c81e189f3920ec0bbb6
jso2601 <jso2601@iolani.org>
2025-02-17 15:40:13 -1000

    4 motor gearbox for the elevator

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 112;
-  public static final String GIT_SHA = "c3a2de65b78de8cad25bee543b07f0ebe32b2f03";
-  public static final String GIT_DATE = "2025-02-11 20:52:43 EST";
+  public static final int GIT_REVISION = 114;
+  public static final String GIT_SHA = "fd79d64a58e75b39e353e32fc245af8b97bea739";
+  public static final String GIT_DATE = "2025-02-13 11:40:03 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-02-12 21:53:08 EST";
-  public static final long BUILD_UNIX_TIME = 1739415188552L;
+  public static final String BUILD_DATE = "2025-02-17 19:11:58 EST";
+  public static final long BUILD_UNIX_TIME = 1739837518915L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -302,35 +302,30 @@ public class RobotContainer {
     //     .onTrue(CoralCommands.scoreCoral(Goal.SCOREL2, elevator, CoralManipulator, arm));
 
     operatorController
-                .x()
-                .whileTrue(
-                    Commands.startEnd(
-                        () -> elevator.setGoal(Goal.SCOREL2),
-                        () -> elevator.setGoal(Goal.STOW)));
+        .x()
+        .whileTrue(
+            Commands.startEnd(
+                () -> elevator.setGoal(Goal.SCOREL2), () -> elevator.setGoal(Goal.STOW)));
 
     operatorController
-                .a()
-                .whileTrue(
-                    Commands.startEnd(
-                        ()->elevator.setGoal(Goal.SCOREL3),
-                        ()->elevator.setGoal(Goal.STOW)));
+        .a()
+        .whileTrue(
+            Commands.startEnd(
+                () -> elevator.setGoal(Goal.SCOREL3), () -> elevator.setGoal(Goal.STOW)));
 
     operatorController
-                .b()
-                .onTrue(
-                    Commands.sequence(
-                        elevator.getSetpointCommand(Goal.SCOREL4),
-                        Commands.runOnce(()-> arm.setGoal(Goalposition.SCOREL4)))
-                );
+        .b()
+        .onTrue(
+            Commands.sequence(
+                elevator.getSetpointCommand(Goal.SCOREL4),
+                Commands.runOnce(() -> arm.setGoal(Goalposition.SCOREL4))));
 
     operatorController
-                .b()
-                .onFalse(
-                    Commands.sequence(
-                        Commands.run(()->arm.setGoal(Goalposition.DEFAULT)),
-                        Commands.runOnce(()->elevator.setGoal(Goal.STOW))
-                    )
-                );
+        .b()
+        .onFalse(
+            Commands.sequence(
+                Commands.run(() -> arm.setGoal(Goalposition.DEFAULT)),
+                Commands.runOnce(() -> elevator.setGoal(Goal.STOW))));
 
     operatorController.leftBumper().whileTrue(CoralManipulator.getCommand(sensor, LED));
     operatorController.leftBumper().onFalse(Commands.runOnce(() -> CoralManipulator.setOutake(0)));
@@ -342,10 +337,10 @@ public class RobotContainer {
                 () -> CoralManipulator.setOutake(-0.5), () -> CoralManipulator.setOutake(0)));
 
     operatorController
-                .leftTrigger(0.2)
-                .whileTrue(Commands.startEnd(
-                    ()->CoralManipulator.setOutake(1),
-                    ()->CoralManipulator.setOutake(0)));
+        .leftTrigger(0.2)
+        .whileTrue(
+            Commands.startEnd(
+                () -> CoralManipulator.setOutake(1), () -> CoralManipulator.setOutake(0)));
 
     // MANUAL CONTROLS
   }
