2dfc76d75b1fb1630935c0f85668c9af6a77d200
leungbrayden <leungbrayden@gmail.com>
2025-04-01 15:26:06 -1000

    swerve retuning after motor replacement

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "FRC-2025-Reefscape";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 143;
-  public static final String GIT_SHA = "64a8de4ffec9397dda4e6897abe503287da7a211";
-  public static final String GIT_DATE = "2025-04-01 15:53:55 EDT";
+  public static final int GIT_REVISION = 144;
+  public static final String GIT_SHA = "43610c52b4048d72be25fa2bdfdbf6608c295a89";
+  public static final String GIT_DATE = "2025-04-01 16:49:02 EDT";
   public static final String GIT_BRANCH = "auto-alignment";
-  public static final String BUILD_DATE = "2025-04-01 16:38:15 EDT";
-  public static final long BUILD_UNIX_TIME = 1743539895437L;
+  public static final String BUILD_DATE = "2025-04-01 21:17:34 EDT";
+  public static final long BUILD_UNIX_TIME = 1743556654861L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
